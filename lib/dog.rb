de830ece20a1a868fe31dc3f7574b3627ef5c3f3
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2022-10-19 08:53:05 +0300

    completed

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+
+
+
+class Dog
+    def bark
+        puts 'Woof!'
+    end
+    def sit
+        puts  "The Dog is sitting"
+    end
+end
+
+ scooby = Dog.new
+ scooby.bark
+
+
+ alex= Dog.new 
+ alex.sit
